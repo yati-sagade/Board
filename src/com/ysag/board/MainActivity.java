bf28f90ce7ed1bd169d5afcdef53c2d81582285d
Yati Sagade <yati.sagade@gmail.com>
2014-04-27 20:34:14 +0530

    Working smooth sound, but breaks when movement is fast

--- src/com/ysag/board/MainActivity.java
+++ src/com/ysag/board/MainActivity.java
@@ -10,45 +10,16 @@ import android.graphics.*;
 import android.support.v4.view.*;
 
 public class MainActivity extends Activity {
-    private final int duration = 3; // seconds
     private final int sampleRate = 8000; // hz
-    private final int numSamples = sampleRate * duration; // seconds
-    private final double[] samples = new double[numSamples];
-    private final double frequency = 440; // hz
-
-    private final double minFreq = frequency;
-
-    private final double maxFreq = 2 * minFreq;
-
-    // The current frequency of the tone.
-    private volatile double currentFrequency = minFreq;
-
-    // Whether to keep playing or not.
-    private volatile boolean playFlag = true;
-
-    private volatile long idx = 0;
-
-    private volatile double[] contSamples = new double[1000];
-
-    private volatile byte[] contMusic = new byte[2000];
-
-
-    // 16-bit PCM data
-    private final byte[] generatedSound = new byte[2 * numSamples];
-
-    private final double TWO_PI = 2 * Math.PI;
-
-    private final double _R = sampleRate / frequency;
-
     private static final String tag = "board";
+    private float screenWidth = 0.0f;
+    private float screenHeight = 0.0f;
+    private final float minFreq = 440;
+    private final float maxFreq = 2 * minFreq;
 
-    private double screenWidth = 0.0;
-    
-    private double screenHeight = 0.0;
+    private volatile float frequency = 0.0f;
 
-    private GestureDetectorCompat detector;
-
-    Handler handler = new Handler();
+    private volatile boolean isPlaying = false;
 
     private final int buffSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
     
@@ -59,23 +30,24 @@ public class MainActivity extends Activity {
                                                          buffSize,
                                                          AudioTrack.MODE_STREAM);
 
-
-    private final LinkedBlockingQueue<byte[]> queue = new LinkedBlockingQueue<byte[]>();
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // Create a GestureDetector with a Context and a
-        // GestureDetector.OnGestureListener
-        detector = new GestureDetectorCompat(this, new BoardGestureListener());
-
         Point size = new Point();
         getWindowManager().getDefaultDisplay().getSize(size);
-        screenWidth = (double) size.x;
-        screenHeight = (double) size.y;
+        screenWidth = (float) size.x;
+        screenHeight = (float) size.y;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (isPlaying) {
+            stopPlaying();
+        }
     }
 
     @Override
@@ -119,91 +91,39 @@ public class MainActivity extends Activity {
 
     private void updateFrequency(MotionEvent event) {
         final float x = event.getX();
-        final double frac = x / screenWidth;
-        currentFrequency = minFreq + frac * (maxFreq - minFreq);
+        final float frac = x / screenWidth;
+        frequency = minFreq + frac * (maxFreq - minFreq);
     }
 
     private void startPlaying() {
-        final Thread thread = new Thread(new Runnable(){
-            public void run() {
-                while (playFlag) {
-                    for (int i = 0; i < contSamples.length; ++i) {
-                        contSamples[i] = Math.sin(TWO_PI * currentFrequency * i / sampleRate);
-                    }
-                    int jdx = 0;
-                    for (double sample : contSamples) {
-                        short val = (short) (32767 * sample);
-                        contMusic[jdx++] = (byte) (val & 0x00ff);
-                        contMusic[jdx++] = (byte) ((val & 0xff00) >>> 8);
-                    }
-                    audioTrack.write(contMusic, 0, contMusic.length);
-                }
-            }
-        });
-        playFlag = true;
+        isPlaying = true;
         audioTrack.play();
-        thread.start();
+        new PlayerTask().execute();
     }
 
     private void stopPlaying() {
-        playFlag = false;
-        idx = 0;
+        isPlaying = false;
         audioTrack.stop();
     }
 
-    public void generateTone(View view) {
-        Log.d("board", "Here in generateTone()");
-        final Thread thread = new Thread(new Runnable() {
-            public void run() {
-                _generateTone();
-                handler.post(new Runnable() {
-                    public void run() {
-                        playSound();
-                    }
-                });
-            }
-        });
-        Log.d("board", "Created the thread");
-        thread.start();
-    }
+    private class PlayerTask extends AsyncTask<Void, Void, Void> {
+        float increment;
+        float angle = 0;
+        short[] samples = new short[512];
 
-    private void _generateTone() {
-        final double end = 2 * frequency;
-        for (int i = 0; i < numSamples; ++i) {
-            final double frac = i / (double) numSamples;
-            final double f = frequency + frac * (end - frequency);
-            samples[i] = Math.sin(TWO_PI * i * f / sampleRate);
-        }
-
-        // Convert the sample buffer into a 16 bit PCM sound array.
-        int idx = 0;
-        for (final double sample : samples) {
-            // Scale to maximum amplitude
-            final short val = (short) (sample * 32767);
-
-            // In 16-bit WAV PCM, the the first byte is the lower byte.
-            generatedSound[idx++] = (byte) (val & 0x00ff);
-            generatedSound[idx++] = (byte) ((val & 0xff00) >>> 8);
-        }
-    }
-
-    private void playSound() {
-        final AudioTrack audioTrack = new AudioTrack(
-                AudioManager.STREAM_MUSIC,
-                sampleRate,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, generatedSound.length,
-                AudioTrack.MODE_STATIC
-        );
-        audioTrack.write(generatedSound, 0, generatedSound.length);
-        audioTrack.play();
-    }
-
-    public static class BoardGestureListener extends GestureDetector.SimpleOnGestureListener {
         @Override
-        public boolean onScroll(MotionEvent ev1, MotionEvent ev2, float dx, float dy) {
-            Log.d(tag, "onScroll(): " + dx + ", " + dy);
-            return true;
+        protected Void doInBackground(Void... params) {
+            while (isPlaying) {
+                for (int i = 0; i < samples.length; ++i) {
+                    increment = (float) (2 * Math.PI) * frequency / sampleRate;
+                    samples[i] = (short)((float)Math.sin(angle) * Short.MAX_VALUE);
+                    angle += increment;
+                    angle %= (2.0f * (float) Math.PI);
+                }
+                audioTrack.write(samples, 0, samples.length);
+            }
+            return null;
         }
     }
+
 }
